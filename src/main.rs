f9f8c2acf1f696b24231693d17a5d248ada05218
apakhomov <apkhmv@gmail.com>
2024-09-02 16:45:06 +0300

    Write first line of code to check the setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-
 fn main() {
-
+    println!("{}", parse())
 }
 
-fn bar() {
+fn parse() -> String {
+    return "parsed".to_string();
+}
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let parsed = parse();
+        assert_eq!("parsed".to_string(), parsed)
+    }
 }
