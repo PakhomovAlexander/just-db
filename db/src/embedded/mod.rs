af4f4943fac13671945707933338cc3c15e1ce35
apakhomov <apkhmv@gmail.com>
2024-12-06 19:37:41 +0300

    Now...

--- db/src/embedded/mod.rs
+++ db/src/embedded/mod.rs
@@ -14,6 +14,12 @@ pub struct Db {
     optimizer: Optimizer,
 }
 
+impl Default for Db {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Db {
     pub fn new() -> Self {
         let mut catalog = Catalog::mem();
