4dac3cbcd9f2c49bb50d89ceab5a1e700002fe4a
apakhomov <apkhmv@gmail.com>
2024-12-06 19:36:49 +0300

    Now it works!

---
+++ db/src/embedded/mod.rs
@@ -0,0 +1,80 @@
+use std::rc::Rc;
+
+use crate::{
+    analyzer::Analyzer,
+    catalog::{Catalog, DataType, TableSchemaBuilder},
+    optimizer::{Optimizer, StorageEngine, Tuple, Val},
+    parser::{lexer::Lexer, Parser},
+};
+
+pub struct Db {
+    catalog_rc: Rc<Catalog>,
+    storage_rc: Rc<StorageEngine>,
+    analyzer: Analyzer,
+    optimizer: Optimizer,
+}
+
+impl Db {
+    pub fn new() -> Self {
+        let mut catalog = Catalog::mem();
+        let mut storage = StorageEngine::mem();
+
+        let ts = TableSchemaBuilder::public()
+            .table("table1")
+            .col("name", DataType::Int)
+            .col("address", DataType::Int)
+            .col("email", DataType::Int)
+            .build();
+        let _ = catalog.register_table(&ts);
+
+        storage.insert(
+            "table1",
+            vec![
+                Tuple::new(vec![
+                    ("name", Val::Int(1)),
+                    ("address", Val::Int(2)),
+                    ("email", Val::Int(10)),
+                ]),
+                Tuple::new(vec![
+                    ("name", Val::Int(111)),
+                    ("address", Val::Int(2222)),
+                    ("email", Val::Int(10000)),
+                ]),
+                Tuple::new(vec![
+                    ("name", Val::Int(13211)),
+                    ("address", Val::Int(2)),
+                    ("email", Val::Int(10)),
+                ]),
+                Tuple::new(vec![
+                    ("name", Val::Int(1)),
+                    ("address", Val::Int(2)),
+                    ("email", Val::Int(10)),
+                ]),
+            ],
+        );
+
+        let catalog_rc = Rc::new(catalog);
+        let storage_rc = Rc::new(storage);
+
+        let optimizer = Optimizer::new(Rc::clone(&catalog_rc), Rc::clone(&storage_rc));
+
+        Self {
+            catalog_rc,
+            storage_rc,
+            analyzer: Analyzer::new(),
+            optimizer,
+        }
+    }
+
+    pub fn run_query(&self, query: &str) -> Vec<Tuple> {
+        let lexer = Lexer::new(query);
+        let mut parser = Parser::new(lexer);
+        let analyzer = Analyzer::new();
+
+        let l_plan = analyzer.analyze(&parser.parse());
+
+        let mut p_plan = self.optimizer.optimize(l_plan);
+
+        p_plan.execute_all()
+    }
+}
---
+++ tui/src/components/editor.rs
@@ -0,0 +1,73 @@
+use color_eyre::Result;
+use edtui::{EditorEventHandler, EditorMode, EditorState};
+use ratatui::prelude::*;
+use tokio::sync::mpsc::UnboundedSender;
+
+use super::Component;
+use crate::{action::Action, config::Config};
+
+#[derive(Default)]
+pub struct Editor {
+    command_tx: Option<UnboundedSender<Action>>,
+    config: Config,
+    editor_state: EditorState,
+    editor_event_handler: EditorEventHandler,
+}
+
+impl Editor {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Component for Editor {
+    fn name(&self) -> &str {
+        "editor"
+    }
+
+    fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<()> {
+        self.command_tx = Some(tx);
+        Ok(())
+    }
+
+    fn register_config_handler(&mut self, config: Config) -> Result<()> {
+        self.config = config;
+        Ok(())
+    }
+
+    fn update(&mut self, action: Action) -> Result<Option<Action>> {
+        match action {
+            Action::ToEditingMode => {
+                self.editor_state.mode = EditorMode::Insert;
+            }
+            Action::Typed(event) => {
+                self.editor_event_handler
+                    .on_key_event(event, &mut self.editor_state);
+
+                let mut text = String::new();
+                for line in &self.editor_state.lines.flatten(&Some('\n')) {
+                    text.push_str(&line.to_string());
+                }
+                // maybe flatten?
+
+                self.command_tx
+                    .as_ref()
+                    .unwrap()
+                    .send(Action::TextUpdated(text))?;
+            }
+            _ => {}
+        }
+        Ok(None)
+    }
+
+    fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
+        use edtui::{EditorTheme, EditorView};
+
+        let editor = EditorView::new(&mut self.editor_state)
+            .theme(EditorTheme::default())
+            .wrap(true);
+
+        frame.render_widget(editor, area);
+        Ok(())
+    }
+}
